e3d7e3d71913b10d9b4ac3f05d3100a8f630e6d8
Rubén Chiquin <randreu28@gmail.com>
2023-12-26 13:55:16 +0100

    Creates a simple GO server

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+// Define a new type for the JSON object
+type Response struct {
+	Value int    `json:"value"`
+	Test  string `json:"test"`
+}
+
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	numStr := req.URL.Query().Get("number")
+
+	if numStr == "" {
+		http.Error(res, "Missing 'number' query parameter", http.StatusBadRequest)
+		return
+	}
+
+	num, err := strconv.Atoi(numStr)
+
+	if err != nil {
+		http.Error(res, "'number' query parameter must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	response := Response{
+		Value: num,
+		Test:  "Yes, it works!",
+	}
+
+	json.NewEncoder(res).Encode(response)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
