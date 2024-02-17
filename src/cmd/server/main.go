96818d1b5c254439ed0e54c1b174c56df3bd8265
Rubén Chiquin <randreu28@gmail.com>
2024-02-17 15:24:26 +0100

    Implements a gracefull server stop

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"payment_service/routes"
+	db "payment_service/utils/database"
 	"payment_service/utils/env"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -18,7 +24,6 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
-
 	router := mux.NewRouter()
 
 	router.HandleFunc("/health", routes.Health)
@@ -26,7 +31,40 @@ func main() {
 	router.HandleFunc("/accounts/{id}", routes.GetAccountDetails).Methods("GET")
 
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
-	fmt.Println("Server listening to port", port)
-	http.ListenAndServe(":"+port, loggedRouter)
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: loggedRouter,
+	}
 
+	log.Println("Server listening on port", port)
+	go func() {
+		err := server.ListenAndServe()
+
+		if err == http.ErrServerClosed {
+			log.Println("Shutting down server...")
+			return
+		}
+
+		if err != nil {
+			panic(fmt.Sprintf("Error starting server: %s\n", err))
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+	<-stopChan // Blocks main() until signal is received
+
+	// Create a deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		fmt.Printf("Server Shutdown Failed:%+v", err)
+	}
+
+	db.Close()
+
+	fmt.Println("Server and database connection gracefully closed")
 }
