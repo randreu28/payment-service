35ceb20ebbffb58062c1135ae76c7e765556a453
Rubén Chiquin <randreu28@gmail.com>
2024-03-03 12:07:26 +0100

    Completes the accounts tests

--- src/routes/accounts_test.go
+++ src/routes/accounts_test.go
@@ -3,25 +3,84 @@ package routes
 import (
 	"net/http"
 	"net/http/httptest"
+	db "payment_service/utils/database"
 	env "payment_service/utils/env"
 	"strings"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestCreateNewAccount(t *testing.T) {
 	env.Load()
 
-	body := strings.NewReader(`{"owner":"John Doe"}`)
-	req, err := http.NewRequest("POST", "/account", body)
+	database := db.Open()
+	db.CleanupDB(database)
+	db.PopulateDB(database, 100)
+	defer database.Close()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/accounts", CreateNewAccount).Methods("POST")
+
+	body := strings.NewReader(`{"owner":"ruben"}`)
+	req, err := http.NewRequest("POST", "/accounts", body)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+}
+
+func TestGetAccountDetails(t *testing.T) {
+	env.Load()
+	database := db.Open()
+	db.CleanupDB(database)
+	db.PopulateDB(database, 100)
+	defer database.Close()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/accounts/{id}", GetAccountDetails).Methods("GET")
+
+	req, err := http.NewRequest("GET", "/accounts/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	res := httptest.NewRecorder()
-	handler := http.HandlerFunc(CreateNewAccount)
+	r.ServeHTTP(res, req)
 
-	handler.ServeHTTP(res, req)
+	t.Log(res.Body.String())
+
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
+
+func TestDeleteAccount(t *testing.T) {
+	env.Load()
+
+	database := db.Open()
+	db.CleanupDB(database)
+	db.PopulateDB(database, 100)
+	defer database.Close()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/accounts/{id}", DeleteAccount).Methods("DELETE")
+
+	req, err := http.NewRequest("DELETE", "/accounts/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
 
 	if status := res.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
